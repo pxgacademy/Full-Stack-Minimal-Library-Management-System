08923d6275d0343ef047686577bd24561616a9f7
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-02 16:49:27 +0600

    added global error handler

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
+import { errorResponse } from "./utils/response";
 
 const app = express();
 
@@ -14,6 +15,29 @@ app.use((req: Request, res: Response) => {
   res
     .status(404)
     .json({ success: false, message: "Route not found", data: null });
+});
+
+// eslint-disable-next-line
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    if (error.name === "ValidationError") {
+      errorResponse(res, 400, "Validation failed", {
+        name: error.name,
+        errors: error.errors,
+      });
+    } else
+      errorResponse(res, 500, "Internal server error", {
+        name: error.name,
+        message: error.message,
+      });
+
+    return;
+  }
+
+  errorResponse(res, 500, "Internal server error", {
+    name: "Error",
+    message: "No error was detected",
+  });
 });
 
 export default app;
