0181d9c4ef10b39075497e081a1c371f4ee13e47
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-02 17:10:40 +0600

    created user model

---
+++ backend/src/models/user.models.ts
@@ -0,0 +1,26 @@
+import { model, Schema } from "mongoose";
+import { UserInputs } from "../types/user.types";
+
+const userSchema: Schema<UserInputs> = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: [/.+\@.+\..+/, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password must be at least 8 characters"],
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const User = model<UserInputs>("User", userSchema);
--- backend/src/types/user.types.ts
+++ backend/src/types/user.types.ts
@@ -1,6 +1,6 @@
 import { Document } from "mongoose";
 
-export interface User extends Document {
+export interface UserInputs extends Document {
   name: string;
   email: string;
   password: string;
