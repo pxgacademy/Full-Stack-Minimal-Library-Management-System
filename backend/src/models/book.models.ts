c33b3bac0c66cbffd03ee43c1376f6ded8cb4365
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-02 17:02:43 +0600

    added book model

---
+++ backend/src/models/book.models.ts
@@ -0,0 +1,40 @@
+import { model, Schema } from "mongoose";
+import { BookDocument, BookGenre } from "../types/book.types";
+
+const bookSchema = new Schema<BookDocument>(
+  {
+    title: { type: String, required: [true, "Title is required"], trim: true },
+    author: {
+      type: String,
+      required: [true, "Author is required"],
+      trim: true,
+    },
+    genre: {
+      type: String,
+      enum: {
+        values: Object.values(BookGenre),
+        message: "Enter a valid genre, {VALUE} is invalid",
+      },
+      required: [true, "Genre is required"],
+      uppercase: true,
+    },
+    isbn: {
+      type: String,
+      required: [true, "ISBN is required"],
+      unique: [true, "ISBN is already exist, enter an unique ISBN"],
+    },
+    description: { type: String, default: "" },
+    copies: {
+      type: Number,
+      required: [true, "Number of copies is required"],
+      min: [0, "Copies cannot be negative, must be at least 1"],
+    },
+    available: { type: Boolean, required: [true, "Availability is required"] },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const Book = model<BookDocument>("Book", bookSchema);
