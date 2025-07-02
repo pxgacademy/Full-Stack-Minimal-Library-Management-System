dea49202757bf2f58a36a4db07f635143f20472b
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-02 16:56:48 +0600

    added borrow types of ts

---
+++ backend/src/types/borrow.types.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+import { BookResponse } from "./book.types";
+
+export interface BorrowBookInputs {
+  user: Types.ObjectId;
+  book: Types.ObjectId;
+  quantity: number;
+  dueDate: Date;
+}
+
+export interface BorrowBookResponse {
+  _id: string;
+  book: string | BookResponse;
+  quantity: number;
+  dueDate: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
