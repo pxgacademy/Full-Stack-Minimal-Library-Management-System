d28daa04cb4df7ab45d944ba61c49e76af7e46c6
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-07-03 12:47:53 +0600

    set-up redux

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
+import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routes/routes";
+import { store } from "./redux/store";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
